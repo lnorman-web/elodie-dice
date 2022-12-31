651e5950e55665244ef99dabf7cade31999a0818
lnorman-web <121566100+lnorman-web@users.noreply.github.com>
2022-12-31 16:16:06 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("HI ELODIE")
+    basic.showNumber(randint(1, 6))
+    basic.pause(2000)
+    basic.showString("AGAIN?")
+})
+basic.forever(function () {
+	
+})
