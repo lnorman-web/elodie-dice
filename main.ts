6a945555a8863b20393e4bb0e5602e210df57e99
lnorman-web <121566100+lnorman-web@users.noreply.github.com>
2022-12-31 16:19:58 +0000

    added waits

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("HI ELODIE")
     basic.showNumber(randint(1, 6))
     basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000)
     basic.showString("AGAIN?")
 })
 basic.forever(function () {
